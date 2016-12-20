5bfbc63dddbe4db82f0b57b0ed8861b7fafa8d91
Chris <cmwingo@gmail.com>
2016-12-20 15:45:57 -0800

    Unity project

---
+++ Assets/Scripts/CoinCounter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[ExecuteInEditMode]
+
+public class CoinCounter : MonoBehaviour
+{
+    public static int coinValue;
+    public Text scoreText;
+
+    void Start ()
+    {
+        coinValue = 0;
+    }
+
+    void Update ()
+    {
+        scoreText.text = coinValue.ToString();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Coin")
+        {
+            coinValue = coinValue + 1;
+        }
+    }
+}
